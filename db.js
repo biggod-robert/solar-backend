88583c7043bd153bb1c828bae96f70dcb0a67162
robert <robertmoor2003@gmail.com>
2025-09-25 16:23:02 -0500

    Ractualizacion 2 db.js y se elimina el codigo duplicado

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
-
-// No se usa dotenv en producción; Railway inyecta sus propias vars
+// en esta caso No se usa dotenv en producción; Railway inyecta sus propias vars
 const { Pool } = require('pg');
 
 // DEBUG: imprime la URL para confirmar que le llega la de Railway
@@ -17,18 +16,4 @@ pool
 
 module.exports = pool;
 
-const { Pool } = require('pg');
 
-console.log('📡 DATABASE_URL:', process.env.DATABASE_URL);
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
-
-pool
-  .query('SELECT NOW()')
-  .then(res => console.log('✅ Conectado a PostgreSQL remoto:', res.rows[0]))
-  .catch(err => console.error('❌ Error al conectar con PostgreSQL:', err));
-
-module.exports = pool;
