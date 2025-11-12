c38e4f1d4c6c0b4c9dde887fb867ec23dd12a76a
robert <robertmoor2003@gmail.com>
2025-11-12 17:30:53 -0500

    modificacion de codigo cotizaciones: separacion llamadas router.get/post/put

--- routes/cotizaciones.js
+++ routes/cotizaciones.js
@@ -1,1 +1,35 @@
-const{Router:Router}=require("express"),{body:body}=require("express-validator"),validarCampos=require("../middlewares/validarCampos"),validarJWT=require("../middlewares/validarJWT"),{registrarCotizacion:registrarCotizacion,obtenerCotizaciones:obtenerCotizaciones,obtenerEstadisticas:obtenerEstadisticas,actualizarCotizacion:actualizarCotizacion}=require("../controllers/cotizacionesController"),router=Router();router.get("/",obtenerCotizaciones),router.get("/todas",obtenerCotizaciones),router.get("/estadisticas",validarJWT,obtenerEstadisticas),router.post("/registrar",validarJWT,validarCampos(["nombre","cedula","ciudad","panel","totalCotizado"]),registrarCotizacion),router.put("/:id",validarJWT,body("nombre").optional().notEmpty().withMessage("nombre no puede quedar vacío"),body("cedula").optional().notEmpty().withMessage("cédula no puede quedar vacía"),body("ciudad").optional().notEmpty().withMessage("ciudad no puede quedar vacía"),body("panel").optional().notEmpty().withMessage("panel no puede quedar vacío"),body("totalCotizado").optional().isNumeric().withMessage("totalCotizado debe ser numérico"),validarCampos,actualizarCotizacion),module.exports=router;
+const express = require("express");
+const { body } = require("express-validator");
+const validarCampos = require("../middlewares/validarCampos");
+const validarJWT = require("../middlewares/validarJWT");
+const {
+  registrarCotizacion,
+  obtenerCotizaciones,
+  obtenerEstadisticas,
+  actualizarCotizacion,
+} = require("../controllers/cotizacionesController");
+
+const router = express.Router();
+
+router.get("/", obtenerCotizaciones);
+router.get("/todas", obtenerCotizaciones);
+router.get("/estadisticas", validarJWT, obtenerEstadisticas);
+router.post(
+  "/registrar",
+  validarJWT,
+  validarCampos(["nombre", "cedula", "ciudad", "panel", "totalCotizado"]),
+  registrarCotizacion
+);
+router.put(
+  "/:id",
+  validarJWT,
+  body("nombre").optional().notEmpty().withMessage("nombre no puede quedar vacío"),
+  body("cedula").optional().notEmpty().withMessage("cédula no puede quedar vacía"),
+  body("ciudad").optional().notEmpty().withMessage("ciudad no puede quedar vacía"),
+  body("panel").optional().notEmpty().withMessage("panel no puede quedar vacío"),
+  body("totalCotizado").optional().isNumeric().withMessage("totalCotizado debe ser numérico"),
+  validarCampos,
+  actualizarCotizacion
+);
+
+module.exports = router;
